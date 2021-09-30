8573cb17fd91f64fce6e59b904fb3f50b2ba2766
Катя <1katiavolkova@gmail.com>
2021-10-01 00:06:08 +0300

    Армия моделей

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,5 +2,19 @@
 
 /* @var $this yii\web\View */
 
+use frontend\models\User;
+use frontend\models\Task;
+
 $this->title = 'Taskforce';
-?>
+
+$users = User::find()->one();
+
+print_r('Пользователь с id: '.$users['id'].'<br> Id его задач: ');
+
+$tasks = Task::find()->where(['author_id' => $users['id']])->all();
+
+foreach ($tasks as $task) {
+    $user_task[] = $task['id'];
+}
+print_r(implode(",", $user_task));
+
